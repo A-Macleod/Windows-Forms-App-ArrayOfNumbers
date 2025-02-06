e36ad2bb63dc3c8e2a1cdf159ada61271e3c0e21
A-Macleod <0308180@uhi.ac.uk>
2025-02-06 18:17:26 +0000

    Button1_Click/ Added error handing and validation to create Array of a given input size
    Creates an Array of given size when the integer is parsed from TextBox1. If the integer can not be parsed catch error message

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,11 +41,14 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.Enter = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(481, 49);
+            this.button1.Location = new System.Drawing.Point(488, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -55,7 +58,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(562, 49);
+            this.button2.Location = new System.Drawing.Point(569, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -64,7 +67,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(643, 49);
+            this.button3.Location = new System.Drawing.Point(650, 12);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
@@ -73,7 +76,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(481, 87);
+            this.button4.Location = new System.Drawing.Point(488, 50);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 3;
@@ -82,7 +85,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(562, 87);
+            this.button5.Location = new System.Drawing.Point(569, 50);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 4;
@@ -91,7 +94,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(643, 87);
+            this.button6.Location = new System.Drawing.Point(650, 50);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 5;
@@ -100,7 +103,7 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(481, 126);
+            this.button7.Location = new System.Drawing.Point(488, 89);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(75, 23);
             this.button7.TabIndex = 6;
@@ -109,7 +112,7 @@
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(562, 126);
+            this.button8.Location = new System.Drawing.Point(569, 89);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(75, 23);
             this.button8.TabIndex = 7;
@@ -118,7 +121,7 @@
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(643, 126);
+            this.button9.Location = new System.Drawing.Point(650, 89);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(75, 23);
             this.button9.TabIndex = 8;
@@ -134,9 +137,8 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(57, 90);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(209, 20);
+            this.textBox1.Size = new System.Drawing.Size(307, 20);
             this.textBox1.TabIndex = 11;
-            this.textBox1.Text = "Placeholder";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox2
@@ -145,24 +147,56 @@
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(580, 111);
+            this.textBox2.Size = new System.Drawing.Size(628, 111);
             this.textBox2.TabIndex = 12;
-            this.textBox2.Text = "Read Only TextBox\r\n\r\nMaybe we could use richTextbBox to format this properly\r\n";
+            this.textBox2.Text = "ArrayOfNumbers Application.\r\n\r\nInsert the size of the array you want to create in" +
+    " a numeric value, then press ENTER..";
             // 
             // Enter
             // 
-            this.Enter.Location = new System.Drawing.Point(272, 90);
+            this.Enter.Location = new System.Drawing.Point(370, 90);
             this.Enter.Name = "Enter";
             this.Enter.Size = new System.Drawing.Size(77, 20);
             this.Enter.TabIndex = 13;
             this.Enter.Text = "Enter";
             this.Enter.UseVisualStyleBackColor = true;
+            this.Enter.Click += new System.EventHandler(this.Enter_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(488, 127);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(569, 127);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(651, 126);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 16;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(737, 359);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
             this.Controls.Add(this.Enter);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -197,6 +231,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button Enter;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,21 +11,91 @@ using System.Windows.Forms;
 
 namespace ArrayOfNumbers
 {
+    /// <summary>
+    /// Form class for the application GUI
+    /// </summary>
     public partial class Form1 : Form
     {
-        //private ArrayOfNumbers newArrayOfNumbers;   // Declare a refference to ArrayOfNumbers to be used by Form1 Class Methods
+        private ArrayOfNumbers newArray;   // Declare a refference to ArrayOfNumbers to be used by Form1 Class Methods
+        
 
         public Form1()
         {
             InitializeComponent();
         }
 
+
+        /// <summary>
+        /// OnClick Method to create array at given size. Takes size value from TextBox1. Catch error exception and output
+        /// error message if the try parse from textBox1 does not contain a number
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
 
+            try {
+
+                string input = textBox1.Text;
+                int result = int.Parse(input);
+
+                if (result > 0) {
+
+                    newArray = new ArrayOfNumbers(result);
+                    textBox2.Text = $"New Array of size '{result}' Created.";
+                    clearTextBox();
+
+                } else {
+                    textBox2.Text = $"Please Enter a Number for the Array Size that is Greater than 0";
+                }
+
+
+            } catch (Exception ex) {
+
+                textBox2.Text = $"Please Enter a Number for the Array Size, then press ENTER.. + {ex.Message}";
+
+            }
+  
         }
 
+
+        /// <summary>
+        /// A Method to clear the textBox1 text on the application GUI
+        /// </summary>
+        private void clearTextBox()
+        {
+            textBox1.Text = null;
+        }
+
+
+
+
+    
+
         /// Add a ReadMe Doc.. week 3 notes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }   // EO Form1 class
 
@@ -34,8 +105,16 @@ namespace ArrayOfNumbers
     /// </summary>
     public class ArrayOfNumbers
     {
-        private int[] _arrayOfNumbers;    // Private field integer Array 
+        private int[] _array;    // Private field integer Array 
 
+        /// <summary>
+        /// Property to get the array object
+        /// </summary>
+        public int[] Array 
+        {
+            get { return _array; }
+            //set { _array = value; }
+        }
 
         /// <summary>
         /// Constructor #1
@@ -46,10 +125,10 @@ namespace ArrayOfNumbers
         public ArrayOfNumbers(int sizeOfArray)
         {
 
-            _arrayOfNumbers = new int[sizeOfArray];
+            _array = new int[sizeOfArray];
 
             for (int i = 0; i < sizeOfArray; i++) { // Loop through and set each index value to 0
-                _arrayOfNumbers[i] = 0;
+                _array[i] = 0;
             }
 
         }
@@ -57,41 +136,41 @@ namespace ArrayOfNumbers
 
         /// <summary>
         /// Constructor #2 
-        /// Accepts array of integers as input and creates a copy of it for internal storage.
-        /// Create a new array and copy each element.
+        /// Accepts array of integers as input and creates a copy of it for internal storage
+        /// Create a new array and copy each element
         /// </summary>
         /// <param name="arrayToCopy">The initial object reference</param>
         public ArrayOfNumbers(int[] arrayToCopy)
         {
 
-            _arrayOfNumbers = new int[arrayToCopy.Length];  // Creates a new _arrayOfNumbers object using an arrayToCopys Length to determine its size
+            _array = new int[arrayToCopy.Length];  // Creates a new _arrayOfNumbers object using an arrayToCopys Length to determine its size
 
             for (int i = 0; i < arrayToCopy.Length; i++) {  // Loop through and set the new array indexs to the same as the copied array 
-                _arrayOfNumbers[i] = arrayToCopy[i];
+                _array[i] = arrayToCopy[i];
             }
 
         }
 
 
         /// <summary>
-        /// A Method to Return the value at a specificed index position.
+        /// A Method to Return the value at a specificed array index position
         /// </summary>
         /// <param name="indexNumber">The index number</param>
         /// <returns>The integer value of the index number</returns>
         public int ReturnElementValue(int indexNumber)
         {
-            return _arrayOfNumbers[indexNumber];
+            return _array[indexNumber];
         }
 
 
         /// <summary>
-        /// A Method to Set an element at the specificed index to the given value 
+        /// A Method to Set an element at the specificed array index to the given value 
         /// </summary>
         /// <param name="indexNumber">The number of the index</param>
         /// <param name="value">The value you want to insert</param>
         public void SetElementValue(int indexNumber, int value)
         {
-            _arrayOfNumbers[indexNumber] = value;
+            _array[indexNumber] = value;
         }
 
 
@@ -104,9 +183,9 @@ namespace ArrayOfNumbers
         {
             int largest = 0;
 
-            for(int i = 0; i < _arrayOfNumbers.Length; i++) {   // Loop through the array
-                if (_arrayOfNumbers[i] > largest) {             // If the index value is greater than var largest
-                    largest = _arrayOfNumbers[i];               // set var largest to that array index value
+            for(int i = 0; i < _array.Length; i++) {   // Loop through the array
+                if (_array[i] > largest) {             // If the index value is greater than var largest
+                    largest = _array[i];               // set var largest to that array index value
                 }
             }
 
@@ -129,10 +208,10 @@ namespace ArrayOfNumbers
             int a = 0;
             int b = 0;
 
-            for (int i = 0; i < _arrayOfNumbers.Length; ++i) {
+            for (int i = 0; i < _array.Length; ++i) {
 
-                  a = _arrayOfNumbers[elementA];
-                  b = _arrayOfNumbers[elementB];
+                  a = _array[elementA];
+                  b = _array[elementB];
 
             }
 
@@ -160,7 +239,7 @@ namespace ArrayOfNumbers
         /// <returns>The length of the array</returns>
         public int ReturnCount()
         {
-            return _arrayOfNumbers.Length;
+            return _array.Length;
         }
 
 
@@ -172,9 +251,9 @@ namespace ArrayOfNumbers
         {
             int sum = 0;
 
-            for (int i = 0; i < _arrayOfNumbers.Length; i++) {
+            for (int i = 0; i < _array.Length; i++) {
 
-                sum += _arrayOfNumbers[i];
+                sum += _array[i];
 
             }
 
@@ -205,8 +284,8 @@ namespace ArrayOfNumbers
         {
             string[] value = { " ," };
 
-            for (int i = 0; i < _arrayOfNumbers.Length; i++) {
-                value[_arrayOfNumbers.Length] += _arrayOfNumbers[i].ToString();   
+            for (int i = 0; i < _array.Length; i++) {
+                value[_array.Length] += _array[i].ToString();   
             }
 
             return value.ToString();
@@ -224,9 +303,9 @@ namespace ArrayOfNumbers
             // v = k x v
             int value = 0;
 
-            for(int i = 0; i < _arrayOfNumbers.Length; i++) {   // Loop through and multiply each index value by the scalar value 
-                _arrayOfNumbers[i] *= scalarValue;
-                value = _arrayOfNumbers[i];
+            for(int i = 0; i < _array.Length; i++) {   // Loop through and multiply each index value by the scalar value 
+                _array[i] *= scalarValue;
+                value = _array[i];
 
             }
 
@@ -242,12 +321,12 @@ namespace ArrayOfNumbers
         public int ReturnAddConstant()
         {
             
-            const int constantValue = 10;  // A Constant is a var whose value cannot be changed after assignemnt
+            const int constantValue = 100;  // A Constant is a var whose value cannot be changed after assignemnt
             int value = 0;
 
-            for (int i = 0; i < _arrayOfNumbers.Length; i++) {
-                _arrayOfNumbers[i] += constantValue;
-                value = _arrayOfNumbers[i];
+            for (int i = 0; i < _array.Length; i++) {
+                _array[i] += constantValue;
+                value = _array[i];
 
             }
 
